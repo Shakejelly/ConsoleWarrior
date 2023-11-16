0b2547e548674e91438c3e7c2537ce5581c866e4
fredr <Feffe_@live.se>
2023-11-16 23:19:59 +0100

    .

--- ConsoleWarrior/Program.cs
+++ ConsoleWarrior/Program.cs
@@ -7,27 +7,27 @@
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-            Functions.Print("\t\t\tWelcome to Cånzål Wharrjorz. Choose a username.");
-            Console.Write("\t\t\t");
+            Functions.Print("\t\tWelcome to Cånzål Wharrjorz. What is your name?.");
+            Console.Write("\t\tUsername: ");
             string userName = Functions.CursorReadLine();
             Console.Clear();
             //Functions.Print($"\t\t\tHello {userName}, you are about to begin your journey. But before that, choose your class.");
-            
-            string[] classOption = { "Warrior", "Bowman", "Sorcerer" };
-            int playerClass = Functions.OptionsNavigation(classOption,$"\t\t\tHello {userName}, you are about to begin your journey. But before that, choose your class.");
+            Functions.Print($"\t\tHello {userName}, you are about to begin your journey. But before that, choose your class.");
+            string[] classOption = { "\t\tWarrior", "\t\tBowman", "\t\tSorcerer" };
+            int playerClass = Functions.OptionsNavigation(classOption, $"\t\tHello {userName}, you are about to begin your journey. But before that, choose your class.");
 
             switch (playerClass)
             {
-                case 1:
-                    Functions.Print("\t\t\tYou have chosen Warrior.");
+                case 0:
+                    Functions.Print("\t\tYou have chosen Warrior.");
                     
                      break;
-                case 2:
-                    Functions.Print("\t\t\tYou have chosen Bowman.");
+                case 1:
+                    Functions.Print("\t\tYou have chosen Bowman.");
                     
                     break;
-                case 3:
-                    Functions.Print("\t\t\tYou have chosen Sorcerer");
+                case 2:
+                    Functions.Print("\t\tYou have chosen Sorcerer");
                     
                     break;
             }
