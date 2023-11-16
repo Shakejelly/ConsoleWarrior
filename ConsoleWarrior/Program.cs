96996466696bdd8c6aed7e2d874eac0012bd6b5e
fredr <Feffe_@live.se>
2023-11-16 21:59:02 +0100

    More methods in Functions

--- ConsoleWarrior/Program.cs
+++ ConsoleWarrior/Program.cs
@@ -1,46 +1,36 @@
 ﻿namespace ConsoleWarrior
+    
 {
+    using ConsoleWarrior.Utilities;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Print("Welcome to Cånzål Wharrjorz. Choose a username.");
-            string userName = Console.ReadLine();
+            Console.CursorVisible = false;
+            Functions.Print("\t\t\tWelcome to Cånzål Wharrjorz. Choose a username.");
+            Console.Write("\t\t\t");
+            string userName = Functions.CursorReadLine();
             Console.Clear();
-            Print($"Hello {userName}, you are about to begin your journey. But before that, choose your class.");
-            Console.WriteLine("1. Warrior\t\t\t2. Bowman\t\t\t3. Sorcerer");
-
-            string playerClass = Console.ReadLine();
+            //Functions.Print($"\t\t\tHello {userName}, you are about to begin your journey. But before that, choose your class.");
+            
+            string[] classOption = { "Warrior", "Bowman", "Sorcerer" };
+            int playerClass = Functions.OptionsNavigation(classOption,$"\t\t\tHello {userName}, you are about to begin your journey. But before that, choose your class.");
 
             switch (playerClass)
             {
-                case "1":
-                    Print("You have chosen Warrior.");
-                    playerClass = "Warrior";
+                case 1:
+                    Functions.Print("\t\t\tYou have chosen Warrior.");
+                    
                      break;
-                    case "2":
-                    Print("You have chosen Bowman.");
-                    playerClass = "Bowman";
+                case 2:
+                    Functions.Print("\t\t\tYou have chosen Bowman.");
+                    
                     break;
-                    case "3":
-                    Print("You have chosen Sorcerer");
-                    playerClass = "Sorcerer";
+                case 3:
+                    Functions.Print("\t\t\tYou have chosen Sorcerer");
+                    
                     break;
             }
-            
-            
-                
-
-        }
-        public static void Print(string text, int speed = 40)
-        {
-            foreach (char c in text)
-            {
-                Console.Write(c);
-                System.Threading.Thread.Sleep(speed);
-            }
-            Console.WriteLine();
         }
     }
-   
 }
---
+++ ConsoleWarrior/Utilities/Functions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWarrior.Utilities
+{
+    internal class Functions
+    {
+        public static void Print(string text, int speed = 40)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                System.Threading.Thread.Sleep(speed);
+            }
+            Console.WriteLine();
+        }
+        public static string CursorReadLine()
+        {
+            string input;
+            Console.CursorVisible = true;
+            input = Console.ReadLine();
+            Console.CursorVisible = false;
+            return input;
+        }
+        public static int OptionsNavigation(string[] options, string phrase)
+        {
+            int menuSelection = 0;
+
+            // Loops until user presses enter on a choice
+            while (true)
+            {
+                // Clears window and re-prints the sent in phrase on each loop
+                Console.Clear();
+                Console.WriteLine(phrase);
+
+                // Forloop to print all the options 
+                for (int i = 0; i < options.Length; i++)
+                {
+                    // Changes color of the option we've currently selected so when menuSelection is for exemple "2" the second option will turn darkgrey
+                    if (i == menuSelection)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                    }
+
+                    // Prints all the options in the array along with the pointer arrow if on the current selection
+                    Console.Write($"{options[i]}{(menuSelection == i ? " <--" : "")}\n");
+
+                    // Reset color to default
+                    Console.ResetColor();
+                }
+
+                //"Listen" to keystrokes from the user
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                //Handles the arrow keys to move up and down the menu
+                if (key.Key == ConsoleKey.UpArrow && menuSelection > 0)
+                {
+                    menuSelection--;
+                }
+                else if (key.Key == ConsoleKey.DownArrow && menuSelection < options.Length - 1)
+                {
+                    menuSelection++;
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    return menuSelection;
+                }
+            }
+        }
+    }
+}
+
